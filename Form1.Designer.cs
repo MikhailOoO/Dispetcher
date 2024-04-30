5de438a31752444137625a64f4253cc74712392a
Mikhail <Mikhail@DESKTOP-UFT0E4S>
2024-05-01 01:26:26 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace disOfTskGrph
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.button2 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.dataGridView3 = new System.Windows.Forms.DataGridView();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.counterCPU = new System.Diagnostics.PerformanceCounter();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.tabPage3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.counterCPU)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Location = new System.Drawing.Point(15, 17);
+            this.tabControl1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(727, 671);
+            this.tabControl1.SizeMode = System.Windows.Forms.TabSizeMode.FillToRight;
+            this.tabControl1.TabIndex = 0;
+            this.tabControl1.Enter += new System.EventHandler(this.tabControl1_Enter);
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.button2);
+            this.tabPage1.Controls.Add(this.dataGridView1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 31);
+            this.tabPage1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.tabPage1.Size = new System.Drawing.Size(719, 636);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Информация о системе";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(6, 7);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(132, 32);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Обновить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 46);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(710, 576);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.dataGridView2);
+            this.tabPage2.Location = new System.Drawing.Point(4, 31);
+            this.tabPage2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.tabPage2.Size = new System.Drawing.Size(719, 636);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Производительность";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            this.tabPage2.Enter += new System.EventHandler(this.tabPage2_Enter);
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.AllowUserToAddRows = false;
+            this.dataGridView2.AllowUserToDeleteRows = false;
+            this.dataGridView2.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(6, 7);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.ReadOnly = true;
+            this.dataGridView2.RowHeadersVisible = false;
+            this.dataGridView2.RowHeadersWidth = 51;
+            this.dataGridView2.RowTemplate.Height = 24;
+            this.dataGridView2.Size = new System.Drawing.Size(707, 622);
+            this.dataGridView2.TabIndex = 0;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.dataGridView3);
+            this.tabPage3.Location = new System.Drawing.Point(4, 31);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(719, 636);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Процессы";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            this.tabPage3.Enter += new System.EventHandler(this.tabPage3_Enter);
+            // 
+            // dataGridView3
+            // 
+            this.dataGridView3.AllowUserToAddRows = false;
+            this.dataGridView3.AllowUserToDeleteRows = false;
+            this.dataGridView3.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView3.Location = new System.Drawing.Point(6, 6);
+            this.dataGridView3.Name = "dataGridView3";
+            this.dataGridView3.ReadOnly = true;
+            this.dataGridView3.RowHeadersVisible = false;
+            this.dataGridView3.RowHeadersWidth = 51;
+            this.dataGridView3.RowTemplate.Height = 24;
+            this.dataGridView3.Size = new System.Drawing.Size(707, 623);
+            this.dataGridView3.TabIndex = 2;
+            this.dataGridView3.ColumnHeaderMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.DataGridView3_ColumnHeaderMouseClick);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 500;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 1200;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // counterCPU
+            // 
+            this.counterCPU.CategoryName = "Processor";
+            this.counterCPU.CounterName = "% Processor Time";
+            this.counterCPU.InstanceName = "_Total";
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker1_ProgressChanged);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 22F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(754, 704);
+            this.Controls.Add(this.tabControl1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Добрый вечер, я ДИСПЕТЧЕР!";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.tabPage2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.tabPage3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.counterCPU)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.DataGridView dataGridView3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Diagnostics.PerformanceCounter counterCPU;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,238 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Management;
+using System.Windows.Forms;
+
+namespace disOfTskGrph
+{
+    public partial class Form1 : Form
+    {
+        DataTable dt;
+        private List<SortInfo> sortInfoList = new List<SortInfo>();
+        int rowIndex = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Start();
+            timer2.Start();
+            try
+            {
+                this.Text = "Компьютер: " + Registry.LocalMachine.OpenSubKey(@"SYSTEM\CurrentControlSet\Control\ComputerName\ComputerName").GetValue("ComputerName").ToString() +
+                    " / " + "ОС: " + Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\Windows NT\CurrentVersion").GetValue("ProductName").ToString();
+            }
+            catch (Exception e)
+            {
+                this.Text = "Ошибка обращения в реестр";
+                Console.Write(e.Message);
+            }
+        }
+
+
+        private void tabControl1_Enter(object sender, EventArgs e)
+        {
+
+            dataGridView1.DataSource = getInfoFir();
+        }
+
+        private void tabPage2_Enter(object sender, EventArgs e)
+        {
+            dataGridView2.DataSource = getInfoSec();
+        }
+
+        private void tabPage3_Enter(object sender, EventArgs e)
+        {
+            dataGridView3.DataSource = getInfoThi();
+            foreach (SortInfo sortInfo in sortInfoList)
+            {
+                DataGridViewColumn column = dataGridView3.Columns[sortInfo.ColumnName];
+                if (column != null)
+                {
+                    if (sortInfo.SortOrder == SortOrder.Ascending)
+                    {
+                        dataGridView3.Sort(column, System.ComponentModel.ListSortDirection.Ascending);
+                    }
+                    else if (sortInfo.SortOrder == SortOrder.Descending)
+                    {
+                        dataGridView3.Sort(column, System.ComponentModel.ListSortDirection.Descending);
+                    }
+                }
+            }
+            rowIndex = dataGridView3.FirstDisplayedScrollingRowIndex;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            dataGridView2.DataSource = getInfoFir();
+        }
+
+
+        private DataTable getInfoFir()
+        {
+            DataTable dataTable = new DataTable();
+            dataTable.Columns.Add("Компонент", typeof(string));
+            dataTable.Columns.Add("Название", typeof(string));
+            dataTable.Rows.Add("Процессор", Registry.LocalMachine.OpenSubKey(@"HARDWARE\DESCRIPTION\System\CentralProcessor\0").GetValue("ProcessorNameString").ToString());
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher("SELECT * FROM Win32_PhysicalMemory");
+            long totalMemory = 0;
+            foreach (ManagementObject obj in searcher.Get())
+            {
+                totalMemory += Convert.ToInt64(obj["Capacity"]);
+            }
+            dataTable.Rows.Add("Объем оперативной памяти (MB)", (totalMemory / 1024 / 1024).ToString());
+            dataGridView1.DataSource = dataTable;
+            searcher = new ManagementObjectSearcher("SELECT * FROM Win32_DiskDrive");
+            foreach (ManagementObject obj in searcher.Get())
+            {
+                dataTable.Rows.Add("Диск " + obj["Model"] + " (GB)", (float.Parse(obj["Size"].ToString()) / 1024 / 1024 / 1024).ToString());
+            }
+
+            searcher = new ManagementObjectSearcher("SELECT * FROM Win32_VideoController");
+            foreach (ManagementObject obj in searcher.Get())
+            {
+                dataTable.Rows.Add("Видеокарта ", obj["Name"]);
+            }
+            return dataTable;
+        }
+        private DataTable getInfoSec()
+        {
+
+            DataTable dataTable = new DataTable();
+            dataTable.Columns.Add("Компонент", typeof(string));
+            dataTable.Columns.Add("Параметр 1", typeof(string));
+            dataTable.Columns.Add("Параметр 2", typeof(string));
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher("SELECT * FROM Win32_PerfFormattedData_PerfOS_Processor WHERE Name='_Total'");
+            dataTable.Rows.Add("Загрузка процессора", counterCPU.NextValue().ToString() + "%", "");
+            searcher = new ManagementObjectSearcher("SELECT * FROM Win32_OperatingSystem");
+            foreach (ManagementObject obj in searcher.Get())
+            {
+                long totalMemory = Convert.ToInt64(obj["TotalVisibleMemorySize"]);
+                long freeMemory = Convert.ToInt64(obj["FreePhysicalMemory"]);
+                long usedMemory = totalMemory - freeMemory;
+                dataTable.Rows.Add("Загрузка памяти (MB)", (usedMemory / 1024).ToString() + " (занято)", (freeMemory / 1024).ToString() + " (свободно)");
+            }
+
+            searcher = new ManagementObjectSearcher("SELECT * FROM Win32_LogicalDisk WHERE DriveType=3");
+            foreach (ManagementObject obj in searcher.Get())
+            {
+                dataTable.Rows.Add("Диск " + obj["DeviceID"] + " (GB)", (Convert.ToUInt64(obj["Size"]) / (1024 * 1024 * 1024)).ToString() + " (занято)", (Convert.ToUInt64(obj["FreeSpace"]) / (1024 * 1024 * 1024)).ToString() + " (свободно)");
+            }
+            return dataTable;
+        }
+        private DataTable getInfoThi() {
+            DataTable dataTable = new DataTable();
+            dataTable.Columns.Add("Процесс", typeof(string));
+            dataTable.Columns.Add("Нагрузка на процессор (%)", typeof(double));
+            dataTable.Columns.Add("Память (MB)", typeof(double));
+            var processes = Process.GetProcesses();
+            double memoryUsage = 0;
+            double totalCpuTime = 0;
+            foreach (Process process in processes)
+            {
+                try
+                {
+                    totalCpuTime += process.TotalProcessorTime.TotalMilliseconds;
+                }
+                catch (Win32Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                catch (Exception exc)
+                {
+                    Console.WriteLine($"Ошибка при получении информации о процессе {process.ProcessName}: {exc.Message}");
+                }
+            }
+            double cpuUsage = 0;
+            foreach (Process process in processes)
+            {
+                try
+                {
+                    cpuUsage = Math.Round((process.TotalProcessorTime.TotalMilliseconds / totalCpuTime) * 100, 2);
+                    memoryUsage = Math.Round(Convert.ToDouble(process.PrivateMemorySize64)/1024/1024, 2);
+                    dataTable.Rows.Add(process.ProcessName, cpuUsage, memoryUsage);
+                }
+                catch (Win32Exception ex)
+                {
+                    Console.WriteLine($"Ошибка при получении информации о процессе {process.ProcessName}: {ex.Message}");
+                }
+                catch (Exception exc)
+                {
+                    Console.WriteLine($"Ошибка при получении информации о процессе {process.ProcessName}: {exc.Message}");
+                }
+            }
+            return dataTable;
+        }
+
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            rowIndex = dataGridView3.FirstDisplayedScrollingRowIndex;
+            if (tabControl1.SelectedIndex == 2 && !backgroundWorker1.IsBusy) {
+                backgroundWorker1.RunWorkerAsync();
+            }
+            
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            if (tabControl1.SelectedIndex == 1)
+            {
+                dataGridView2.DataSource = getInfoSec();
+            }
+        }   
+
+        private void DataGridView3_ColumnHeaderMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            DataGridViewColumn clickedColumn = dataGridView3.Columns[e.ColumnIndex];
+            SortInfo sortInfo = new SortInfo
+            {
+                ColumnName = clickedColumn.Name,
+                SortOrder = dataGridView3.SortOrder
+            }; 
+            sortInfoList.Add(sortInfo);
+        }
+
+        private class SortInfo
+        {
+            public string ColumnName { get; set; }
+            public SortOrder SortOrder { get; set; }
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            dt = getInfoThi();
+        }
+
+        private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            dataGridView3.DataSource = dt;
+            dataGridView3.FirstDisplayedScrollingRowIndex = rowIndex;
+            foreach (SortInfo sortInfo in sortInfoList)
+            {
+                DataGridViewColumn column = dataGridView3.Columns[sortInfo.ColumnName];
+                if (column != null)
+                {
+                    if (sortInfo.SortOrder == SortOrder.Ascending)
+                    {
+                        dataGridView3.Sort(column, System.ComponentModel.ListSortDirection.Ascending);
+                    }
+                    else if (sortInfo.SortOrder == SortOrder.Descending)
+                    {
+                        dataGridView3.Sort(column, System.ComponentModel.ListSortDirection.Descending);
+                    }
+                }
+            }
+            
+        }
+    }
+}
